4f6a01d650e23eece758f0f2860d7354d6c0ffa7
Михаил Минченко <t9052585903@gmail.com>
2020-10-26 13:47:04 +0300

    feat: add header to new project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
 import React from 'react';
-import Header from './components/Header/Header.jsx';
-import Main from './components/Main/Main.jsx';
-import Advantages from './components/Advantages/Advantages.jsx';
-import Questions from './components/Questions/Questions.jsx';
-import Footer from './components/Footer/Footer.jsx';
+import { createGlobalStyle } from 'styled-components';
+import { Navbar } from './components/Navbar';
 
+const GlobalStyle = createGlobalStyle`
+    * {
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+    }
+
+    body {
+        margin: 0;
+        background-color: #f0f0f0;
+        font-size: 20px;
+        color: black;
+    }
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+    img {
+        max-width: 100%;
+        height: auto
+    }
+
+    ul {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+    }
+    h1,
+    h2,
+    h3
+    {
+        font-family: Pacifico, sans-serif;
+        padding: 0;
+        margin: 0;
+    }
+    p {
+        margin: 0;
+        padding: 0;
+
+    }
+
+`;
 class App extends React.Component {
     render() {
         return (
             <>
-                <Header />
-                <Main />
-                <Advantages />
-                <Questions />
-                <Footer />
+                <GlobalStyle />
+                <Navbar />
             </>
         );
     }
---
+++ src/components/Navbar.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+import './navbar.css';
+import logoImg from '../img/logo.svg';
+import loginImg from '../img/sign.svg';
+
+const NavbarHeader = styled.header`
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 999;
+    height: 80px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    background-color: #299b01;
+    color: white;
+`;
+const Logo = styled.div`
+    display: flex;
+    align-items: center;
+`;
+const LogoText = styled.span`
+    font-size: 24px;
+    margin-left: 15px;
+    font-family: Pacifico, sans-serif;
+`;
+const Img = styled.img`
+    width: 50px;
+`;
+const Login = styled.button`
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    color: white;
+    font-size: 16px;
+    line-height: 19px;
+`;
+const LoginImg = styled.img`
+    width: 32px;
+    display: block;
+    margin: auto;
+    margin-bottom: 3px;
+`;
+
+export const Navbar = () => {
+    return (
+        <>
+            <NavbarHeader>
+                <Logo>
+                    <Img src={logoImg} alt="logo" />
+                    <LogoText>MrDonald's</LogoText>
+                </Logo>
+
+                <Login>
+                    <LoginImg src={loginImg} alt="login" />
+                    войти
+                </Login>
+            </NavbarHeader>
+        </>
+    );
+};
